68487b4a95c8cb17682b09c533282ae94826fca0
Djamel-RB <yhxtalpque@gmail.com>
2025-06-25 22:58:01 +0100

    Update server.js

--- server.js
+++ server.js
@@ -108,5 +108,5 @@ app.get('/auth/callback', async (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
+  console.log(`✅ Server running on https://server-01-uhgr.onrender.com`);
 });
